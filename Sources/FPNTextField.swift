c8be1107def594dba147d3a008cb814644eedaa0
Anmol Malhotra <anmol.malhotra@gamechangesns.com>
2021-05-06 13:03:56 +0530

    Adds extra padding between country code and textfield.

--- Sources/FPNTextField.swift
+++ Sources/FPNTextField.swift
@@ -22,7 +22,7 @@ open class FPNTextField: UITextField {
 
 	/// The size of the leftView
 	private var leftViewSize: CGSize {
-		let width = flagButtonSize.width + getWidth(text: phoneCodeTextField.text!)
+		let width = flagButtonSize.width + getWidth(text: phoneCodeTextField.text!) + 8
 		let height = bounds.height
 
 		return CGSize(width: width, height: height)
